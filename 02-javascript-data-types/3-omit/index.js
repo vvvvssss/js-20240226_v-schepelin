b80a7a967db84180f0fcbe5e9323955be4509d88
Vadim <vadim@MacBook-Pro-Vadim.local>
2024-03-03 17:24:46 +0300

    2 task omit

--- 02-javascript-data-types/3-omit/index.js
+++ 02-javascript-data-types/3-omit/index.js
@@ -5,5 +5,13 @@
  * @returns {object} - returns the new object
  */
 export const omit = (obj, ...fields) => {
-
+  const entri = new Map(Object.entries(obj));
+  const result = {};
+  const ArrFruits = Object.keys(obj);
+  for (const fruit of ArrFruits) {
+    if (entri.has(fruit) && !fields.includes(fruit)) {
+      result[fruit] = entri.get(fruit);
+    }
+  }
+  return result;
 };
