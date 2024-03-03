ed3ea0e59ec824f45d552c7b63946ba7c99e98c8
Vadim <vadim@MacBook-Pro-Vadim.local>
2024-03-03 14:23:38 +0300

    2 task

--- 02-javascript-data-types/1-sort-strings/index.js
+++ 02-javascript-data-types/1-sort-strings/index.js
@@ -4,6 +4,14 @@
  * @param {string} [param="asc"] param - the sorting type "asc" or "desc"
  * @returns {string[]}
  */
-export function sortStrings(arr, param = 'asc') {
-
+export function sortStrings(arr, param = "asc") {
+  let numb = 1;
+  if (param === "desc") {
+    numb = -1;
+  } else {
+    numb = 1;
+  }
+  return [...arr].sort(
+    (a, b) => numb * a.localeCompare(b, "ru", { caseFirst: "upper" })
+  );
 }
