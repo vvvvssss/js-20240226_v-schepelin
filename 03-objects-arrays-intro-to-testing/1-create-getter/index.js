6913e1759000289ee596afbb1d61ea86fa5bbade
Vadim <vadim@MacBook-Pro-Vadim.local>
2024-03-04 18:46:53 +0300

    3 task createGetter

--- 03-objects-arrays-intro-to-testing/1-create-getter/index.js
+++ 03-objects-arrays-intro-to-testing/1-create-getter/index.js
@@ -4,5 +4,15 @@
  * @returns {function} - function-getter which allow get value from object by set path
  */
 export function createGetter(path) {
-
+  const key = path.split(".");
+  return function (obj) {
+    let field = obj[key[0]];
+    for (let i = 1; i < key.length; i++) {
+      if (!field || Object.hasOwnProperty(field, key[i])) {
+        return;
+      }
+      field = field[key[i]];
+    }
+    return field;
+  };
 }
