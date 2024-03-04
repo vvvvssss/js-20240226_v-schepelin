0ca16f7b0394bceba888f2f0262bbae02630c3aa
Vadim <vadim@MacBook-Pro-Vadim.local>
2024-03-04 18:52:51 +0300

    3 task invertObj

--- 03-objects-arrays-intro-to-testing/2-invert-object/index.js
+++ 03-objects-arrays-intro-to-testing/2-invert-object/index.js
@@ -4,5 +4,13 @@
  * @returns {object | undefined} - returns new object or undefined if nothing did't pass
  */
 export function invertObj(obj) {
-
+  if (!obj) return;
+  const newObj = Object.entries(obj);
+  for (let eachObj of newObj) {
+    let empty;
+    empty = eachObj[0];
+    eachObj[0] = eachObj[1];
+    eachObj[1] = empty;
+  }
+  return Object.fromEntries(newObj);
 }
