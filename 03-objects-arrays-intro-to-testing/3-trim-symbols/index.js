029f9f11266fb7dffa1588059c899c57f86cac0d
Vadim <vadim@MacBook-Pro-Vadim.local>
2024-03-05 12:23:43 +0300

    3 task trimSymbols

--- 03-objects-arrays-intro-to-testing/3-trim-symbols/index.js
+++ 03-objects-arrays-intro-to-testing/3-trim-symbols/index.js
@@ -5,5 +5,24 @@
  * @returns {string} - the new string without extra symbols according passed size
  */
 export function trimSymbols(string, size) {
-
+  if (string.length == 0 || size <= 0) return "";
+  if (!size) return string;
+  for (let i = 0; i < string.length; i++) {
+    let countY = 1;
+    for (let y = i + 1; y < string.length; y++) {
+      if (string[i] == string[y] && countY < size) {
+        countY++;
+        continue;
+      }
+      if (string[i] == string[y] && countY == size) {
+        string = string.slice(0, y) + string.slice(y + 1);
+        y -= 1;
+        continue;
+      }
+      if (string[i] != string[y]) {
+        break;
+      }
+    }
+  }
+  return string;
 }
