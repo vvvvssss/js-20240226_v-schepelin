c49c746944c50d6a2c0b1d160f89c2ca5214299b
Vadim <vadim@MacBook-Pro-Vadim.local>
2024-03-05 12:57:58 +0300

    3 task uniq

--- 03-objects-arrays-intro-to-testing/4-uniq/index.js
+++ 03-objects-arrays-intro-to-testing/4-uniq/index.js
@@ -4,5 +4,10 @@
  * @returns {*[]} - the new array with uniq values
  */
 export function uniq(arr) {
-
+  if (!arr || arr.length == 0) return [];
+  const newArr = [];
+  for (let i = 0; i < arr.length; i++) {
+    if (!newArr.includes(arr[i])) newArr.push(arr[i]);
+  }
+  return newArr;
 }
